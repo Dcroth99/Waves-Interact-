d3c50f5cf99cae7d4ed2eb5d6eac5b225964d156
Daniel Roth <danielcharlesroth@gmail.com>
2024-06-27 22:33:42 -1000

    29

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.feed, name='feed'),
+    path('feed/', views.feed, name='feed'),
     path('create/', views.create_post, name='create_post'),
     path('like/<int:post_id>/', views.like_post, name='like_post'),
     path('comment/<int:post_id>/', views.comment_post, name='comment_post'),
