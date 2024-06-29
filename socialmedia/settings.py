171262878d5d8dcd41a5fe5cf8161d40281f1a5b
Daniel Roth <danielcharlesroth@gmail.com>
2024-06-28 18:44:00 -1000

    look

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = "socialmedia.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['/media/'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -163,8 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
+#STATIC_URL = "/static/"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'users/media/')
