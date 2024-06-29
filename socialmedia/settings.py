2ba356f16c90d54f5ca626d8e5bec4d412076f92
Daniel Roth <danielcharlesroth@gmail.com>
2024-06-28 20:12:49 -1000

    debug2

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -56,7 +56,7 @@ os.makedirs(logs_dir, exist_ok=True)
 SECRET_KEY = "django-insecure-dyvv+5xh_bidjsyg&6)5#zu(9r^am*=3w8emewmkoy@l493zsi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nameless-ocean-69439.herokuapp.com',  'localhost', '127.0.0.1']
 
